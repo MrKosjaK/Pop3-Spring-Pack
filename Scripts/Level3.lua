521015ea3f182a4589e00936f0ec2f3340e7efa5
MrKosjaK <misterkosiak@gmail.com>
2022-03-06 00:18:25 +0300

    LUA script for level 3

---
+++ Scripts/Level3.lua
@@ -0,0 +1,20 @@
+import(Module_Game);
+import(Module_System);
+import(Module_PopScript);
+import(Module_Defines);
+
+local POPSCRIPT_USER_DIFFICULTY_INDEX = 1;
+local current_game_difficulty = get_game_difficulty();
+
+function OnSave(save_data)
+  --todo
+end
+
+function OnLoad(load_data)
+  --todo
+end
+
+function OnTurn()
+  SET_USER_VARIABLE_VALUE(TRIBE_RED, POPSCRIPT_USER_DIFFICULTY_INDEX, current_game_difficulty);
+  SET_USER_VARIABLE_VALUE(TRIBE_GREEN, POPSCRIPT_USER_DIFFICULTY_INDEX, current_game_difficulty);
+end
